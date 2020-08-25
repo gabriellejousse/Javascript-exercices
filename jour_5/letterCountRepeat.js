a491ec3e2530f68755084209990d101aede78ddb
gabriellejousse <gabrielle.jousse@gmail.com>
2020-08-25 14:24:31 +0200

    sauvegarde 25/08

---
+++ jour_5/letterCountRepeat.js
@@ -0,0 +1,12 @@
+
+var result = 0;
+function letterCountRepeat(str){
+
+    for( i = 0; i < str.length; i++){
+        console.log(str[i]);
+        var objStr = {
+            'a' : result+=1,
+        }
+    }
+}
+console.log(letterCountRepeat('Hello apple pie'));
